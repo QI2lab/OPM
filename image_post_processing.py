07fc45912fd25865e9ca4d5e3f2dac70eea82fe8
Peter Brown <ptbrown1729@gmail.com>
2021-05-19 11:24:17 -0700

    corrected bugs

--- image_post_processing.py
+++ image_post_processing.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 import numpy as np
 from numba import njit, prange
 
@@ -234,7 +235,7 @@ def perform_flat_field(flat_field,dark_field,stack):
 
     return corrected_stack.compute()
 
-   def mv_decon(image,ch_idx,dr,dz):
+def mv_decon(image,ch_idx,dr,dz):
     '''
     Perform deconvolution using Microvolution API. To do: implement reading known PSF from disk. Return deconvolved image.
 
