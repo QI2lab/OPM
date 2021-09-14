corrected bug in filter size calculation for x-direction

localize_skewed.py
@@ -487,20 +487,24 @@ def get_skewed_roi_size(sizes, theta, dc, dstep, ensure_odd=True):
     return [n0, n1, n2]
 
 
-def get_skewed_roi(center, imgs, x, y, z, sizes):
+def get_skewed_roi(center, imgs, coords, sizes):
     """
+    Given a center value (not necessarily aligned to the coordinates), find the closest region of interest (ROI)
+    centered around that point.
 
     :param float center: [cz, cy, cx] in same units as x, y, z
-    :param x:
-    :param y:
-    :param z:
-    :param int sizes: [n0, n1, n2]
-    :param shape: shape of full image ... todo: should infer from x, y, z
-    :return:
+    :param coords: a tuple of coordinates (z, y, x), where z, y, and x are broadcastable to the same shape as imgs.
+    These coordinates are supplied as produced by get_skewed_coords()
+    :param list[int] sizes: [n0, n1, n2], the size of the desired ROI in number of pixels along the skewed coordinate
+    directions. This can be calculated with the help of get_skewed_roi_size()
+    :return roi, img_roi, x_roi, y_roi, z_roi:
     """
+    z, y, x = coords
     shape = imgs.shape
-    i2 = np.argmin(np.abs(x.ravel() - center[2]))
-    i0, i1, _ = np.unravel_index(np.argmin((y - center[1]) ** 2 + (z - center[0]) ** 2), y.shape)
+    i2 = np.argmin(np.abs(x[0, 0, :].ravel() - center[2]))
+    i1 = np.argmin(np.abs(z[0, :, 0] - center[0]))
+    i0 = np.argmin(np.abs(y[:, i1, 0] - center[1]))
+    # i0, i1, _ = np.unravel_index(np.argmin((y - center[1]) ** 2 + (z - center[0]) ** 2), y.shape)
     roi = np.array(rois.get_centered_roi([i0, i1, i2], sizes, min_vals=[0, 0, 0], max_vals=np.array(shape)))
 
     img_roi = rois.cut_roi(roi, imgs)
@@ -509,6 +513,9 @@ def get_skewed_roi(center, imgs, x, y, z, sizes):
     y_roi = y[roi[0]:roi[1], roi[2]:roi[3], :]
     z_roi = z[:, roi[2]:roi[3], :]
     z_roi, y_roi, x_roi = np.broadcast_arrays(z_roi, y_roi, x_roi)
+    # x_roi = rois.cut_roi(roi, x)
+    # y_roi = rois.cut_roi(roi, y)
+    # z_roi = rois.cut_roi(roi, z)
 
     return roi, img_roi, x_roi, y_roi, z_roi
 
@@ -540,19 +547,20 @@ def get_roi_mask(center, max_seps, coords):
 # filtering
 def get_filter_kernel_skewed(sigmas, dc, theta, dstage, sigma_cutoff=2):
     """
-    Gaussian filter kernel
-    :param sigmas:
-    :param dc:
-    :param theta:
-    :param dstage:
-    :param sigma_cutoff:
-    :return:
+    Get gaussian filter convolution kernel in skewed coordinates
+
+    :param sigmas: (sz, sy, sx) in the same units as dc and stage
+    :param dc: pixel size
+    :param theta: angle in radians
+    :param dstage: stage step
+    :param sigma_cutoff: number of standard deviations to include in the filter. This parameter determines the fitler size
+    :return kernel:
     """
     # normalize everything to camera pixel size
     sigma_x_pix = sigmas[2] / dc
     sigma_y_pix = sigmas[2] / dc
     sigma_z_pix = sigmas[0] / dc
-    nk_x = 2 * int(np.round(sigmas[2] * sigma_cutoff)) + 1
+    nk_x = 2 * int(np.round(sigma_x_pix * sigma_cutoff)) + 1
     nk_y = 2 * int(np.round(sigma_y_pix * sigma_cutoff)) + 1
     nk_z = 2 * int(np.round(sigma_z_pix * sigma_cutoff)) + 1
     # determine how large the OPM geometry ROI needs to be to fit the desired filter
@@ -570,15 +578,57 @@ def get_filter_kernel_skewed(sigmas, dc, theta, dstage, sigma_cutoff=2):
     return kernel
 
 
+def get_lapl_gauss_filter_kernel_skewed(sigmas_small, sigmas_large, dc, theta, dstage, sigma_cutoff=2):
+    """
+    Get Laplacian of Gaussian filter convolution kernel in skewed coordinates
+
+    :param sigmas_small: (sz, sy, sx) in the same units as dc and stage
+    :param sigmas_large: (sz, sy, sx) in the same units as dc and stage
+    :param dc: pixel size
+    :param theta: angle in radians
+    :param dstage: stage step
+    :param sigma_cutoff: number of standard deviations to include in the filter. This parameter determines the fitler size
+    :return kernel:
+    """
+    # normalize everything to camera pixel size
+    sxs_pix = sigmas_small[2] / dc
+    sys_pix = sigmas_small[2] / dc
+    szs_pix = sigmas_small[0] / dc
+
+    sxl_pix = sigmas_large[2] / dc
+    syl_pix = sigmas_large[2] / dc
+    szl_pix = sigmas_large[0] / dc
+    nk_x = 2 * int(np.round(sxl_pix * sigma_cutoff)) + 1
+    nk_y = 2 * int(np.round(syl_pix * sigma_cutoff)) + 1
+    nk_z = 2 * int(np.round(szl_pix * sigma_cutoff)) + 1
+    # determine how large the OPM geometry ROI needs to be to fit the desired filter
+    roi_sizes = get_skewed_roi_size([nk_z, nk_y, nk_x], theta, 1, dstage / dc, ensure_odd=True)
+
+    # get coordinates to evaluate kernel at
+    xk, yk, zk = get_skewed_coords(roi_sizes, 1, dstage / dc, theta)
+    xk = xk - np.mean(xk)
+    yk = yk - np.mean(yk)
+    zk = zk - np.mean(zk)
+
+    kl = np.exp(-xk ** 2 / 2 / sxl_pix ** 2 - yk ** 2 / 2 / syl_pix ** 2 - zk ** 2 / 2 / szl_pix ** 2)
+    kl = kl / np.sum(kl)
+    ks = np.exp(-xk ** 2 / 2 / sxs_pix ** 2 - yk ** 2 / 2 / sys_pix ** 2 - zk ** 2 / 2 / szs_pix ** 2)
+    ks = ks / np.sum(ks)
+    kernel = ks - kl
+
+    return kernel
+
+
 # identify peaks
 def get_skewed_footprint(min_sep_allowed, dc, ds, theta):
     """
-    Get footprint for maximum filter
+    Get footprint for maximum filter in skewed coordinates
+
     :param min_sep_allowed: (dz, dy, dx)
-    :param dc:
-    :param ds:
-    :param theta:
-    :return:
+    :param dc: pixel size
+    :param ds: stage step
+    :param theta: angle in radians
+    :return footprint:
     """
     footprint_roi_size = get_skewed_roi_size(min_sep_allowed, theta, dc, ds, ensure_odd=True)
     footprint_form = np.ones(footprint_roi_size, dtype=bool)
@@ -595,6 +645,9 @@ def get_skewed_footprint(min_sep_allowed, dc, ds, theta):
 
 # localization functions
 def localize_radial_symm(img, coords, mode="radial-symmetry"):
+    """
+
+    """
     # todo: check quality of localizations
     if img.ndim != 3:
         raise ValueError("img must be a 3D array, but was %dD" % img.ndim)
@@ -720,6 +773,7 @@ def plot_skewed_roi(fit_params, roi, imgs, theta, x, y, z, init_params=None, sam
                     prefix="", save_dir=None):
     """
     plot results from fit_roi()
+
     :param fit_params:
     :param roi:
     :param imgs:
@@ -1104,8 +1158,10 @@ def localize_skewed(imgs, params, abs_threshold, roi_size, filter_sigma_small, f
 def filter_localizations(fit_params, init_params, coords, fit_dist_max_err, min_spot_sep,
                          sigma_bounds, amp_min=0, dist_boundary_min=(0, 0), mode="skewed"):
     """
-    :param fit_params:
-    :param init_params:
+    Given a collection of fits, determine which fits are plausible localizations based on the fit parameters.
+
+    :param fit_params: nfits x 7 array
+    :param init_params: nfits x 7 array
     :param coords: (z, y, x)
     :param fit_dist_max_err = (dz_max, dxy_max)
     :param min_spot_sep: (dz, dxy) assume points separated by less than this distance come from one spot